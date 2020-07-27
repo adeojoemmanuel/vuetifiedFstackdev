d94f727c7eaecf74687eafe1aad5e731991a6d1c
adeojoemmanuel <emmanuel.adeojo.ibk@gmail.com>
2020-07-27 15:03:56 -0700

    updates

--- src/views/training.vue
+++ src/views/training.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
     <topbar></topbar>
-    <input type="text" name="firstname">
-    <input type="text" name="lastname">
-    <input type="text" name="email">
-    <input type="text" name="phonenumber">
-    <input type="submit" name="submit">
+     <div class="col-lg-6 col-md-12 col-xl-5 col-12">
+                <div class="contact-form-wrapper">
+                  <!-- Start Contact Form -->
+                  <div class="axil-contact-form contact-form-style-1">
+                    <h4 class="title">Get a development quote</h4>
+
+                    <form id="contact-form" method="POST" action="">
+                      <div class="form-group">
+                        <input name="con_name" type="text" />
+                        <label>Name</label>
+                        <span class="focus-border"></span>
+                      </div>
+                      <div class="form-group">
+                        <input name="con_email" type="email" />
+                        <label>Email</label>
+                        <span class="focus-border"></span>
+                      </div>
+                      <div class="form-group">
+                        <input type="text" />
+                        <label>Phone</label>
+                        <span class="focus-border"></span>
+                      </div>
+                      <div class="form-group">
+                        <textarea name="con_message"></textarea>
+                        <label>Your message</label>
+                        <span class="focus-border"></span>
+                      </div>
+
+                      <div class="form-group">
+                        <input type="submit" value="Send message" />
+                        <p class="form-messege"></p>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- End Contact Form -->
+                </div>
+              </div>
     <Footeer></Footeer>
   </div>
 </template>
